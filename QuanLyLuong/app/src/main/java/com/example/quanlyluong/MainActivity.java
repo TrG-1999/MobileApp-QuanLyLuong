097965f3c4dec5e87f7195d95e3aa4c32ca9fe3f
Le Truong Giang <59412424+TrG-1999@users.noreply.github.com>
2021-05-09 18:20:58 +0700

    Process Login and Animation MainActivity
    +Change Layout Login
    +Process Login (default user: admin and pass: admin)
    +add Layout MainActivity
    +Add animation MainActivity

--- QuanLyLuong/app/src/main/java/com/example/quanlyluong/MainActivity.java
+++ QuanLyLuong/app/src/main/java/com/example/quanlyluong/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.quanlyluong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Animation slide_enter, slide_txtheader;
+    ImageView imgmain;
+    TextView txt_header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        //Animation
+        slide_enter = AnimationUtils.loadAnimation(this, R.anim.anim_logomain);
+        slide_txtheader = AnimationUtils.loadAnimation(this, R.anim.anim_txt_header_login);
+        //Reflection
+        imgmain = findViewById(R.id.imgmain);
+        txt_header = findViewById(R.id.txt_header);
+
+        imgmain.setAnimation(slide_enter);
+        txt_header.setAnimation(slide_txtheader);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), login.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.anim_intent_enter,R.anim.anim_intent_exit);
+                finish();
+            }
+        }, 2000);
+
     }
 }
--- QuanLyLuong/app/src/main/java/com/example/quanlyluong/login.java
+++ QuanLyLuong/app/src/main/java/com/example/quanlyluong/login.java
@@ -2,13 +2,51 @@ package com.example.quanlyluong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class login extends AppCompatActivity {
-
+    Button btn_login;
+    TextView txt_alert;
+    TextInputEditText username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+
+        //Reflection
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        txt_alert = findViewById(R.id.txt_alert);
+        btn_login = findViewById(R.id.btn_login);
+        //Action
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                Log.d("login", user);
+                Log.d("login", pass);
+                if ( user.equals("admin") && pass.equals("admin")){
+                    Intent intent = new Intent(getApplicationContext(), Menu.class);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.anim_intent_enter,R.anim.anim_intent_exit);
+                    txt_alert.setText("");
+                    finish();
+                }else{
+                    txt_alert.setText("Xin kiểm tra lại tài khoản và mật khẩu!");
+                }
+
+            }
+        });
     }
 }
